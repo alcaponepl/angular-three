18b822ec496cacfb5ee52b7a6862b994deac0d18
plusnia <plusnia@pgs-soft.com>
2022-07-26 16:00:17 +0200

    Waving flag - example

--- src/app/scenes/scene-five/scene-five.component.ts
+++ src/app/scenes/scene-five/scene-five.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as THREE from 'three';
+import { NgtRenderState } from "@angular-three/core";
 
 @Component({
   selector: 'app-scene-five',
@@ -6,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./scene-five.component.scss']
 })
 export class SceneFiveComponent implements OnInit {
+  uniforms = {
+    uFrequency: {value: new THREE.Vector2(10, 5)},
+    uTime: {value: 0}
+  };
   vertexShader: string = `
         uniform mat4 projectionMatrix;
         uniform mat4 viewMatrix;
@@ -15,11 +21,15 @@ export class SceneFiveComponent implements OnInit {
         attribute vec2 uv;
         varying vec4 vPos;
         varying vec2 vUv;
+        uniform vec2 uFrequency;
+        uniform float uTime;
 
         void main()
         {
 
             vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+            modelPosition.z += sin(modelPosition.x * uFrequency.x + uTime) * 0.1;
+            modelPosition.z += sin(modelPosition.y * uFrequency.y + uTime) * 0.1;
             vec4 viewPosition = viewMatrix * modelPosition;
             vec4 projectedPosition = projectionMatrix * viewPosition;
 
@@ -39,16 +49,26 @@ export class SceneFiveComponent implements OnInit {
 
         void main()
         {
-            float strength = vUv.x;
+            vec3 color;
+            if (vUv.y < 0.5)
+               color = vec3(1.0, 0.0, 0.0);
+            else
+              color = vec3(1.0, 1.0, 1.0);
 
-            gl_FragColor = vec4(vec3(strength), 1.0);
+            gl_FragColor = vec4(color, 1.0);
         }
     `;
 
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  onBeforeRender(tes: any) {
+    // object.position.y = Math.sin(clock.getElapsedTime()) * 0.06;
+    this.uniforms.uTime.value = tes.state.clock.getElapsedTime();
   }
 
 }
